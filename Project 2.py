90a6ded6d89af5ac10397bf177e239734154dce8
Mubarak21 <94360486+Mubarak21@users.noreply.github.com>
2021-11-15 14:57:22 +0400

    Update Project 2.py

--- Project 2.py
+++ Project 2.py
@@ -4,28 +4,39 @@ It asks the user to input the fruit of their choosing and then asks the amount o
 the total amount and displays back the total price for the fruit chosen, the fruit that was chosen and how many where chosen.
 I also added a loop in the program so that it can ask the user continuous until the user is satisfied."""
 
-
-Groceries= [ "Apples" , "Mango","Watermelon"]# The list containing the groceries
-Price= [1.25, 2.9,5]# It contains the prices of the above groceries
+print("Welcome to All day, Everyday Supermarket")
+Groceries= [ "Apples" , "Mango","Watermelon", "Peach", "Banana", "Orange", "Avocado", "Grape", "Pear"]# The list containing the groceries
+Price= [1.25, 2.9,5,1,3,1.5,2,1.4,3]# It contains the prices of the above groceries
 TotalPrice=[]# An empty list to append the prices of the fruits
+Basket=[]
+nFruits=[ ]
 
 def fruits(Groceries, Price):# A function that takes the groceries and TotalPrice and calculates the total price
     for i in range (len(Groceries)):# This loops is because there only 4 fruits in the list so it runs for that many times
         if fruit == Groceries[i]:
                     num= int(input('how many fruits do you want?'))# The number of fruits that you require
+                    nFruits.append(num)
                     Totalfruit= num* Price[i]# It calculates the amount of fruits that you chose
                     TotalPrice.append(Totalfruit)# It adds the prices to the empty list so that at the end of it you can get the total price
                     print(Totalfruit, ' dirhams is the price of', num, fruit)
-    else:
-        print("try again")
-        option= input("Do you wanna try again?, Y or N")
+         else:
+             print("Try Again")
+              fruit= input ('what fruit do you want?, either  Mango, Apples,Watermelon, Peach, Banana, Orange, Avocado, Grape, Pear')
+
+
 choice= "Y"
 while choice != "N":# A loop sothat the user can keep adding this to his list
-            fruit= input ('what fruit do you want?, either  Mango, Apples or Watermelon')# Asks the user what fruit they prefer
+            fruit= input ('what fruit do you want?, either  Mango, Apples,Watermelon, Peach, Banana, Orange, Avocado, Grape, Pear')# Asks the user what fruit they prefer
+            Basket.append(fruit)
             fruits(Groceries, Price)# The function to run the calculation
             choice= input("Do you want to continue?, Y or N")
 
 
 receipt= input("Do you want to print the receipt?, Yes or No")
-if receipt == "Yes":
-                    print("The total amount of all the fruits that you bought is",sum(TotalPrice), " Dirhams")# The final message displaying the Total money need for the groceries
+for i in range(len(Basket)):
+                        if receipt == "Yes":
+                                           print(TotalPrice[i], ' dirhams is the price of', nFruits[i], fruit)
+
+                        else:
+                            print("Thank you for Visiting and Have a good day")
+
